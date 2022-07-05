86dd25895c1a41bbf8acdb00d483f2d48d7c1c1b
ayoubelyousfi <elyousfiayoub12@gmail.com>
2022-07-05 19:40:56 +0100

    delete hibernate config

--- src/main/java/com/util/HibernateUtil.java
+++ src/main/java/com/util/HibernateUtil.java
@@ -5,6 +5,11 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import com.entity.Category;
+import com.entity.Magasin;
+import com.entity.Produit;
+import com.entity.Produit_magasin;
+
 public class HibernateUtil {
 
 	private static final SessionFactory sessionFactory;
@@ -15,7 +20,10 @@ public class HibernateUtil {
 		Configuration config = new Configuration();
 		config.configure();
 		
-		
+		config.addAnnotatedClass(Magasin.class);
+		config.addAnnotatedClass(Produit.class);
+		config.addAnnotatedClass(Category.class);
+		config.addAnnotatedClass(Produit_magasin.class);
 		
 		serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
 		sessionFactory = config.buildSessionFactory(serviceRegistry);
